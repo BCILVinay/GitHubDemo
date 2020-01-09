4736a8388802d7df5cb733220926b9ba8d5af33a
vinay.pandey@barcodeindia.com <vinay.pandey@barcodeindia.com>
2020-01-09 15:07:54 +0530

    fffff

--- WinFormTest/Form2.cs
+++ WinFormTest/Form2.cs
@@ -21,7 +21,7 @@ namespace WinFormTest
         {
             MessageBox.Show("Form Loaded !!!");
             int x = 0;
-            int y = 0;
+          
             int xc = 0;
         }
     }
